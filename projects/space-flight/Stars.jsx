7e68aeb46a1239182f78eb6db7d31adf9a79b562
Juan Cruz Fortunatti <juancfortunatti@gmail.com>
2021-04-12 20:50:57 -0300

    fix: stars and ships contrast

--- projects/space-flight/Stars.jsx
+++ projects/space-flight/Stars.jsx
@@ -5,7 +5,7 @@ export const Stars = ({ scale }) => {
     return Math.log(Math.log(scale(5193000001))) * 1.0313 + 0.2369 || 3.43;
   }, [scale]);
   return (
-    <svg width={2000} height={760} y={-150} style={{ overflow: "hidden" }}>
+    <svg width={2000} height={760} y={-150} style={{ overflow: "hidden", opacity:0.5 }}>
       <g
         transform={`translate(0, 400) scale(${scaleN}) translate(0, -400)`}
         style={{ transition: "all 0.4s ease-out" }}>
