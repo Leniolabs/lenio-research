dd11768fb05061e4b3111fa8f8bfcfefbc300220
Juan Cruz Fortunatti <juancfortunatti@gmail.com>
2021-04-12 20:09:20 -0300

    fix: stars overflow

--- projects/space-flight/Stars.jsx
+++ projects/space-flight/Stars.jsx
@@ -2,10 +2,10 @@ import * as React from "react";
 
 export const Stars = ({ scale }) => {
   const scaleN = React.useMemo(() => {
-    return Math.log(Math.log(scale(5193000001))) * 1.0313 + 0.2369 || 3.4;
+    return Math.log(Math.log(scale(5193000001))) * 1.0313 + 0.2369 || 3.43;
   }, [scale]);
   return (
-    <>
+    <svg width={2000} height={760} y={-150} style={{ overflow: "hidden" }}>
       <g
         transform={`translate(0, 400) scale(${scaleN}) translate(0, -400)`}
         style={{ transition: "all 0.4s ease-out" }}>
@@ -13,7 +13,7 @@ export const Stars = ({ scale }) => {
         <image href={"./stars.svg"} width="1212" height="900" />
         <image href={"./constelations.svg"} width="1212" height="900" />
       </g>
-    </>
+    </svg>
   );
 };
 
