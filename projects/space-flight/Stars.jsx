edf5ee40b765f19db090cdcfb351f3bde0c62d1e
Juan Cruz Fortunatti <juancfortunatti@gmail.com>
2021-04-12 21:04:50 -0300

    fix: resize for big screens

--- projects/space-flight/Stars.jsx
+++ projects/space-flight/Stars.jsx
@@ -1,11 +1,20 @@
 import * as React from "react";
+import { useWindowSize } from "utils/useWindowSize";
 
 export const Stars = ({ scale }) => {
+  const size = useWindowSize();
+
   const scaleN = React.useMemo(() => {
-    return Math.log(Math.log(scale(5193000001))) * 1.0313 + 0.2369 || 3.43;
-  }, [scale]);
+    const s = Math.log(Math.log(scale(5193000001))) * 1.0313 + 0.2369 || 3.43;
+
+    const [width] = size;
+
+    if (1207 * s < width) return width / 1207;
+    return s;ƒ
+  }, [scale, size]);
+
   return (
-    <svg width={2000} height={760} y={-150} style={{ overflow: "hidden", opacity:0.5 }}>
+    <svg height={760} y={-150} style={{ overflow: "hidden", opacity: 0.5 }}>
       <g
         transform={`translate(0, 400) scale(${scaleN}) translate(0, -400)`}
         style={{ transition: "all 0.4s ease-out" }}>
