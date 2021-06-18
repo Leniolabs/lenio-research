cae18b2eddbee65a6502f0fe60a4e377066d579d
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-18 15:56:32 -0300

    Footer style linter

--- components/footer/footer.style.js
+++ components/footer/footer.style.js
@@ -12,7 +12,8 @@ export const FooterLogo = styled.div`
   align-items: center;
   display: flex;
   justify-content: flex-end;
-  div {
+  div,
+  a {
     font-size: 1rem;
     margin: 0 1rem 0 0;
   }
