a43fd3207b21ed03b1f8be1611a4e483dcb114ac
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-18 15:17:41 -0300

    Camel case props

--- projects/space-flight/Controls.jsx
+++ projects/space-flight/Controls.jsx
@@ -4,22 +4,37 @@ import PropTypes from "prop-types";
 export const Controls = ({ x, y, onBack, onForward }) => {
   return (
     <g transform={`translate(${x}, ${y})`}>
-      <rect className="btn-prev" x="-120" y="-40" width="100" height="40" rx="5" fill="#9831ff" onClick={onBack}/>
-      <text x="-95" y="-12"
+      <rect
+        className="btn-prev"
+        x="-120"
+        y="-40"
+        width="100"
+        height="40"
+        rx="5"
+        fill="#9831ff"
+        onClick={onBack}
+      />
+      <text
+        x="-95"
+        y="-12"
         fill="white"
-          font-family="Source Sans Pro"
-          font-weight="600"
-          font-size="24">
-      Prev
-    </text>
-      <rect className="btn-next" y="-40" width="100" height="40" rx="5" fill="#009396" onClick={onForward}/>
-      <text x="24" y="-12"
-        fill="white"
-        font-family="Source Sans Pro"
-        font-weight="600"
-        font-size="24">
-      Next
-    </text>
+        fontFamily="Source Sans Pro"
+        fontWeight="600"
+        fontSize="24">
+        Prev
+      </text>
+      <rect
+        className="btn-next"
+        y="-40"
+        width="100"
+        height="40"
+        rx="5"
+        fill="#009396"
+        onClick={onForward}
+      />
+      <text x="24" y="-12" fill="white" fontFamily="Source Sans Pro" fontWeight="600" fontSize="24">
+        Next
+      </text>
     </g>
   );
 };
