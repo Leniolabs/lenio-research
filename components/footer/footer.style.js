d5d68002cc171cdba1f53e6e4e48b31ae46e9f46
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-18 17:02:44 -0300

    Github link added to footer

--- components/footer/footer.style.js
+++ components/footer/footer.style.js
@@ -6,21 +6,35 @@ export const FooterContainer = styled.footer`
   padding: 2rem;
   text-align: right;
   grid-area: footer;
-`;
-
-export const FooterLogo = styled.div`
-  align-items: center;
   display: flex;
   justify-content: flex-end;
+`;
+
+export const GithubContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  div,
+  a {
+    display: flex;
+    align-items: center;
+    font-size: 1rem;
+    margin: 0 1rem 0 0;
+  }
+  svg {
+    font-size: 0.7rem;
+    margin: 0 1rem 0 0;
+    fill: #fcfcf4;
+  }
+`;
+
+export const CreatedByContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
   div,
   a {
     font-size: 1rem;
     margin: 0 1rem 0 0;
   }
-`;
-
-export const FooterLogoContainer = styled.div`
-  position: absolute;
-  bottom: 125px;
-  right: 20px;
 `;
