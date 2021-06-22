8e8350ff613c6330fa7ccc717b8a5bdaca7c4085
Alfonso <siko2.0@gmail.com>
2021-06-22 12:58:53 -0400

    Linting issues (#51)
    * footer update (#49)
    
    * Feat/footer (#47)
    
    * chore: updated repo name (#48)
    
    Co-authored-by: Nestor Gabriel Gutierrez <44404526+gutierreznestor@users.noreply.github.com>
    
    * fix: linting issues
    
    Co-authored-by: Mariana Beldi <mbeldi@gmail.com>
    Co-authored-by: Nestor Gabriel Gutierrez <44404526+gutierreznestor@users.noreply.github.com>

--- components/project-card/ProjectCard.js
+++ components/project-card/ProjectCard.js
@@ -1,19 +1,28 @@
 import React from "react";
 import Link from "next/link";
 import { Card, CardTitle, CardSubtitle, Button } from "./project-card.style";
+import PropTypes from "prop-types";
 
 const ProjectCard = ({ title, subtitle, imgPreview, description, link }) => {
   return (
     <Card>
       <CardTitle>{title}</CardTitle>
       <CardSubtitle>{subtitle}</CardSubtitle>
-      <img src={imgPreview}></img>
+      <img src={imgPreview} alt="preview of post"></img>
       <p>{description}</p>
       <Link href={link}>
         <Button>Explore Data Visualization</Button>
       </Link>
     </Card>
   );
+};
+
+ProjectCard.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  imgPreview: PropTypes.string,
+  description: PropTypes.string,
+  link: PropTypes.string
 };
 
 export default ProjectCard;
