f2addc331955af3f18200f72e2093a404b11ae52
Jose Banega <banegajose26@gmail.com>
2021-06-29 13:43:39 -0300

    fix: worked on graphic

--- projects/timeline/svg-components/BigGraphic.jsx
+++ projects/timeline/svg-components/BigGraphic.jsx
@@ -9,44 +9,59 @@ const colors = {
   home: "#ff3f55"
 };
 const WIDTH = 40;
-const INITIAL_POSITON = 40.1;
+const INITIAL_X = 80;
+const INITIAL_Y = 10;
 const HEIGHT = 20;
+const LINE_OFFSET = 5;
 export const BigGraphic = ({ companies, selectedDate }) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1080.1 263.1">
       {Object.keys(companies).map((companyId, heightIndex) => {
         const graph = mapDatesToGraphic(companies[companyId]);
-        const yPosition = heightIndex > 0 ? HEIGHT * heightIndex : 0.6;
-        return graph.map(({ date, status }, index) => {
-          const position = index > 0 ? WIDTH * index : INITIAL_POSITON;
-          const linePosition = position + 20;
-          return (
-            <Fragment key={`${companyId}-${index}`}>
-              <rect
-                key={index}
-                width={WIDTH}
-                height={HEIGHT}
-                x={position}
-                y={yPosition}
-                fill={colors[status]}
-                stroke={black}
-                strokeWidth=".3"
-              />
-              {dayjs(selectedDate).isSame(date, "month") && (
-                <motion.line
-                  animate={{ x: [-40, 0] }}
-                  transition={{ duration: 0.8, ease: "easeInOut" }}
-                  x1={linePosition}
-                  x2={linePosition}
-                  y2={yPosition + HEIGHT}
-                  fill="none"
-                  stroke={black}
-                  strokeWidth="2"
-                />
-              )}
-            </Fragment>
-          );
-        });
+        const yPosition = heightIndex > 0 ? HEIGHT * heightIndex + INITIAL_Y : INITIAL_Y;
+        const textPositionY = yPosition + 14;
+        return (
+          <Fragment key={`${companyId}-${heightIndex}-container`}>
+            <text fill="#2a3f55" fontSize="11.2" fontWeight="600" x={20} y={textPositionY}>
+              {companyId
+                .split("")
+                .map((letter, index) => (index ? letter.toLowerCase() : letter.toUpperCase()))
+                .join("")}
+            </text>
+            {graph.map(({ date, status }, index) => {
+              const position = index > 0 ? WIDTH * index + INITIAL_X : INITIAL_X;
+              const linePosition = position + 20;
+              return (
+                <Fragment key={`${companyId}-${heightIndex}-${index}`}>
+                  <rect
+                    key={index}
+                    width={WIDTH}
+                    height={HEIGHT}
+                    x={position}
+                    y={yPosition}
+                    fill={colors[status]}
+                    stroke={black}
+                    strokeWidth=".3"
+                  />
+                  {dayjs(selectedDate).isSame(date, "month") && (
+                    <motion.line
+                      initial={{ x: -40 }}
+                      animate={{ x: 0 }}
+                      transition={{ duration: 0.4, ease: "easeInOut" }}
+                      x1={linePosition}
+                      x2={linePosition}
+                      y1={INITIAL_Y - LINE_OFFSET}
+                      y2={yPosition + HEIGHT + LINE_OFFSET}
+                      fill="none"
+                      stroke={black}
+                      strokeWidth="2"
+                    />
+                  )}
+                </Fragment>
+              );
+            })}
+          </Fragment>
+        );
       })}
     </svg>
   );
