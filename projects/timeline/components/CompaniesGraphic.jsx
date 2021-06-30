b29bd2d512513c84a1c09cad465874367fa6d10f
Jose Banega <banegajose26@gmail.com>
2021-06-29 23:08:05 -0300

    fix: fixed behaviour of CompaniesGraphic animation

--- projects/timeline/components/CompaniesGraphic.jsx
+++ projects/timeline/components/CompaniesGraphic.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { BigGraphic } from "../svg-components/BigGraphic";
 import { LineGraphicText } from "../svg-components/LineGraphicText";
 import { data_graphic } from "../timeline.data";
@@ -12,15 +12,24 @@ const getComapniesData = (data) => {
 const CompaniesGraphic = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const intervalRef = useRef();
-  const [index, setIndex] = useState(0);
   const limit = data_graphic.length - 1;
+  const [index, setIndex] = useState(limit);
+  const [mounted, setMounted] = useState(false);
 
   const nextGraphic = () => {
-    setIndex((prevIndex) => (prevIndex + 1 === limit ? 0 : prevIndex + 1));
+    setIndex((prevIndex) => (prevIndex + 1 >= limit ? 0 : prevIndex + 1));
   };
+
+  useEffect(() => {
+    if (index === limit - 1 && mounted) {
+      onPlayOrStop();
+      setMounted(false);
+    }
+  }, [index, mounted]);
 
   const startInterval = () => {
     nextGraphic();
+    setMounted(true);
     const timelineInterval = setInterval(() => {
       nextGraphic();
     }, 500);
@@ -41,22 +50,66 @@ const CompaniesGraphic = () => {
   return (
     <div>
       <svg viewBox="0 0 1080 14">
-        <text fill="#2a3f55" stroke="#2a3f55" strokeWidth=".2"  fontSize="12" transform="translate(411 10)">
+        <text
+          fill="#2a3f55"
+          stroke="#2a3f55"
+          strokeWidth=".2"
+          fontSize="12"
+          transform="translate(411 10)">
           Most from office
         </text>
-        <rect width="16" height="7.6" x="390" y="3" fill="#2aa881" stroke="#2a3f55" strokeWidth=".3"/>
-        <text fill="#2a3f55" stroke="#2a3f55" strokeWidth=".2" fontSize="12" transform="translate(549.5 10)">
+        <rect
+          width="16"
+          height="7.6"
+          x="390"
+          y="3"
+          fill="#2aa881"
+          stroke="#2a3f55"
+          strokeWidth=".3"
+        />
+        <text
+          fill="#2a3f55"
+          stroke="#2a3f55"
+          strokeWidth=".2"
+          fontSize="12"
+          transform="translate(549.5 10)">
           Hybrid
         </text>
-        <rect width="16" height="7.6" x="528.5" y="3" fill="#ffbf55" stroke="#2a3f55" strokeWidth=".3"/>
-        <text fill="#2a3f55" stroke="#2a3f55" strokeWidth=".2" fontSize="12" transform="translate(635.7 10)">
+        <rect
+          width="16"
+          height="7.6"
+          x="528.5"
+          y="3"
+          fill="#ffbf55"
+          stroke="#2a3f55"
+          strokeWidth=".3"
+        />
+        <text
+          fill="#2a3f55"
+          stroke="#2a3f55"
+          strokeWidth=".2"
+          fontSize="12"
+          transform="translate(635.7 10)">
           Most from home
         </text>
-        <rect width="16" height="7.6" x="614.7" y="3" fill="#ff3f55" stroke="#2a3f55" strokeWidth=".3"/>
+        <rect
+          width="16"
+          height="7.6"
+          x="614.7"
+          y="3"
+          fill="#ff3f55"
+          stroke="#2a3f55"
+          strokeWidth=".3"
+        />
       </svg>
-      <PlayBtn className="btn-graphic" onClick={onPlayOrStop}> {isPlaying ? "⏹️ Stop" : "▶️ Play"}</PlayBtn>
+      <PlayBtn className="btn-graphic" onClick={onPlayOrStop}>
+        {" "}
+        {isPlaying ? "⏹️ Stop" : "▶️ Play"}
+      </PlayBtn>
       {data_graphic.length > 0 && getComapniesData(data_graphic[index])}
-      <svg viewBox="-36 0 997 33"><LineGraphicText></LineGraphicText></svg>
+      <svg viewBox="-36 0 997 33">
+        <LineGraphicText></LineGraphicText>
+      </svg>
     </div>
   );
 };
--- projects/timeline/components/ConclusionCard.jsx
+++ projects/timeline/components/ConclusionCard.jsx
@@ -15,7 +15,7 @@ const ConclusionCard = ({ outcome, color, content, link, variants }) => {
       <blockquote>
         {mainContent} <span>-{author}</span>
       </blockquote>
-      <svg className="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g fill="#d32f2f"><rect width="96" height="512" x="304"/><rect width="96" height="512" x="112"/></g><polygon fill="#f44336" points="400 512 304 512 112 0 208 0"/></svg>      <a href={link}>Source</a>
+      {/* <svg className="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g fill="#d32f2f"><rect width="96" height="512" x="304"/><rect width="96" height="512" x="112"/></g><polygon fill="#f44336" points="400 512 304 512 112 0 208 0"/></svg>      <a href={link}>Source</a> */}
     </Conclusion>
   );
 };
