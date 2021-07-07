68885b0c9c7f1a8918d30db3be0f9d56e7e420ae
Ramiro Spinelli <ramirospinelli92@gmail.com>
2021-07-07 15:04:03 -0300

    feat: Adding react testing library & jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  setupFilesAfterEnv: ["./jest.setup.js"],
+  moduleNameMapper: {
+    "^@components(.*)$": "<rootDir>/components$1",
+    "^@pages(.*)$": "<rootDir>/pages$1",
+    "^@projects(.*)$": "<rootDir>/projects$1"
+  }
+};
