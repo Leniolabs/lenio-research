737ebf63129a3ee303585f55ea011dda91d3329a
sunteago <santi.vallejo@gmail.com>
2021-07-14 09:19:00 -0300

    Add extra edge cases to video player utils

--- projects/video/utils.js
+++ projects/video/utils.js
@@ -8,6 +8,19 @@
  * @returns {string} representing time event. i.e "00:01:40.320"
  */
 export const secondsToTimeEvent = (secs) => {
+  if (typeof secs !== "number") {
+    secs = +secs;
+  }
+
+  if (Number.isNaN(secs) || secs < 0) {
+    secs = 0;
+  }
+
+  // Bigger than 24h
+  if (secs >= 86400) {
+    secs = 86399.999;
+  }
+
   const t = new Date(1970, 0, 1);
   const timestamp = secs.toFixed(3);
   const ms = timestamp.split(".")[1];
@@ -29,11 +42,19 @@ export const secondsToTimeEvent = (secs) => {
  */
 // "00:00:34.320" => 34.32
 export const timeEventToSeconds = (timeEvent) => {
+  timeEvent = timeEvent + "";
+
+  if (timeEvent.length < 8) {
+    timeEvent = "00:00:00";
+  }
+
   const baseDate = new Date(1970, 0, 1);
   const timestampDate = new Date(`1970-01-01 ${timeEvent}`);
   const difference = timestampDate - baseDate;
 
-  return difference / 1000;
+  const parsedSeconds = difference / 1000;
+
+  return Number.isNaN(parsedSeconds) ? timeEventToSeconds("") : parsedSeconds;
 };
 
 /**
@@ -50,11 +71,11 @@ export const getClosestTimeEventIndex = (tsStringList, tsString) => {
   const tsList = tsStringList.map(timeEventToSeconds);
   const positionInSecs = timeEventToSeconds(tsString);
 
-  return tsList.reduce((lastTs, currTs, index) => {
+  return tsList.reduce((lastIndex, currTs, index) => {
     if (currTs < positionInSecs) {
       return index;
     }
 
-    return lastTs;
+    return lastIndex;
   }, 0);
 };
