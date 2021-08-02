f586a8a5dfc58010af9353be945e44d43d817df8
Alfonso Concha <siko2.0@gmail.com>
2021-08-02 18:51:15 -0400

    fix: image path to root

--- projects/olympics/PoleVis.jsx
+++ projects/olympics/PoleVis.jsx
@@ -323,7 +323,7 @@ const PoleVis = () => {
         stroke="transparent"
       />
       <image
-        href={`olympics-salto-blanco.gif`}
+        href={`./olympics-salto-blanco.gif?r=${timestamp}`}
         style={{ pointerEvents: "none" }}
         width="2580"
         height="1127"
