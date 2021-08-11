e0ff922a85b81f9391434a6d02f143ce05c2828f
Alfonso Concha <siko2.0@gmail.com>
2021-08-11 12:52:41 -0400

    feat: added flags to the olympic medals viz

--- projects/olympics/index.js
+++ projects/olympics/index.js
@@ -2,129 +2,11 @@ import * as React from "react";
 import Link from "next/link";
 import { SectionTitle, MainTitle, MainSubTitle, PlayText } from "./style";
 import medals from "./medals.json";
+import { YEAR_OPTIONS } from "./olympics.data";
 import { useTracking } from "analytics/context";
 import { BarChart } from "./BarChart";
 import PoleVis from "./PoleVis";
 import CustomSelect from "@components/select/select";
-
-const YEAR_OPTIONS = [
-  {
-    value: "1896",
-    name: "Athens, Greece"
-  },
-  {
-    value: "1900",
-    name: "Paris, France"
-  },
-  {
-    value: "1904",
-    name: "St. Louis, United States"
-  },
-  {
-    value: "1908",
-    name: "London, Great Britain"
-  },
-  {
-    value: "1912",
-    name: "Stockholm, Sweden"
-  },
-  {
-    value: "1920",
-    name: "Antwerp, Belgium"
-  },
-  {
-    value: "1924",
-    name: "Paris, France"
-  },
-  {
-    value: "1928",
-    name: "Amsterdam,  Netherlands"
-  },
-  {
-    value: "1932",
-    name: "	Los Angeles,  United States"
-  },
-  {
-    value: "1936",
-    name: "Berlin, Germany"
-  },
-  {
-    value: "1948",
-    name: "London, Great Britain"
-  },
-  {
-    value: "1952",
-    name: "Helsinki, Finland"
-  },
-  {
-    value: "1956",
-    name: "Melbourne, Australia and Stockholm, Sweden"
-  },
-  {
-    value: "1960",
-    name: "Rome, Italy"
-  },
-  {
-    value: "1964",
-    name: "Tokyo, Japan"
-  },
-  {
-    value: "1968",
-    name: "Mexico City, Mexico"
-  },
-  {
-    value: "1972",
-    name: "Munich, West Germany"
-  },
-  {
-    value: "1976",
-    name: "Montreal, Canada"
-  },
-  {
-    value: "1980",
-    name: "Moscow, Soviet Union"
-  },
-  {
-    value: "1984",
-    name: "Los Angeles, United States"
-  },
-  {
-    value: "1988",
-    name: "Seoul, South Korea"
-  },
-  {
-    value: "1992",
-    name: "Barcelona, Spain"
-  },
-  {
-    value: "1996",
-    name: "Atlanta, United States"
-  },
-  {
-    value: "2000",
-    name: "Sydney, Australia"
-  },
-  {
-    value: "2004",
-    name: "Athens, Greece"
-  },
-  {
-    value: "2008",
-    name: "Beijing, China"
-  },
-  {
-    value: "2012",
-    name: "London, Great Britain"
-  },
-  {
-    value: "2016",
-    name: "Rio de Janeiro, Brazil"
-  },
-  {
-    value: "2020",
-    name: "Tokyo, Japan"
-  }
-].map((year, idx) => ({ value: year.value, label: `${year.name} - ${year.value}`, index: idx }));
 
 export const Index = () => {
   const [isPlaying, setIsPlaying] = React.useState(false);
@@ -204,10 +86,11 @@ export const Index = () => {
           <CustomSelect
             options={YEAR_OPTIONS}
             selectedOption={YEAR_OPTIONS[dataIndex]}
-            label="Select Date"
+            label=""
             isDisabled={isPlaying}
             onChange={onChangeCallback}
             disabledSearch
+            width={300}
             maxWidth={700}
           />
         </div>
