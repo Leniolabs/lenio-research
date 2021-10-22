dbed18696761cca4140f6bb1029c573b2de91115
Alfonso Concha <alfonso.concha@leniolabs.com>
2021-10-22 11:57:44 -0300

    feat: projection and disclaimer

--- projects/amazon/components/utils.js
+++ projects/amazon/components/utils.js
@@ -1,3 +1,7 @@
+import treeCoverTarget from "./treeCoverTarget";
+import { data } from "../data";
+import newTreeCoverLoss from "./newTreeCoverLoss";
+
 export const colorScale = (value) => {
   if (value > 0.6) return 4;
   if (value > 0.3) return 3;
@@ -8,3 +12,28 @@ export const colorScale = (value) => {
 export const colorDivisions = [1, 2, 3, 4];
 
 export const TOTAL_AREA = 748 * 10000; // each dot is 10000km2 in the Ecuador
+
+const randomizedTreeCoverTarget = treeCoverTarget.map((value) => {
+  if (value === 0.75) {
+    return 0.4 + Math.random() * 0.35;
+  }
+  return value;
+});
+
+export const interpolateTreeCover = (previousTreeCover) => {
+  return previousTreeCover.map((prevValue, index) => {
+    return (randomizedTreeCoverTarget[index] - prevValue) * 0.05 + prevValue;
+  });
+};
+
+const temp = {};
+
+data.forEach(({ year }) => {
+  if (year < 2021) {
+    temp[year] = newTreeCoverLoss[year];
+  } else {
+    temp[year] = interpolateTreeCover(temp[year - 1]);
+  }
+});
+
+export const interpolatedCoverLoss = temp;
